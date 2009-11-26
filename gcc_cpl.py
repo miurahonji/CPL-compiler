bd4a0f20b7d8bf66f8729b909965376e01a6b7c4
Roberto Miura Honji <miurahonji@gmail.com>
2009-11-26 19:27:47 +0000

    Change struture of contents

--- gcc_cpl.py
+++ gcc_cpl.py
@@ -128,14 +128,18 @@ def p_statement_nBegin(t):
 	'''statement : BEGIN nContent nStructure END'''
 	t[0] = '''<HTML>
 	<HEAD><meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	 <TITLE> Untitled </TITLE>
+	 <TITLE>%s</TITLE>
 	 <link rel="stylesheet" type="text/css" href="style/style.css" media="screen" />
-	 </HEAD>''' + t[2] + '''</HTML>'''
+	 </HEAD><BODY>%s</BODY></HTML>''' % (t[2]['newspaper']['title'], '')
 
 #### CONTENT DEFINES ####
 def p_nContent(t):
 	'''nContent : CONTENT LBRACE nNewspaper blocks RBRACE'''
-	t[0] = '<BODY>' + t[3] + '</BODY>'
+	# nNewspaper: return a dictionary with title and date key
+	# blocks: return a list with all block. Each block is a dictionary
+	t[0] = dict()
+	t[0]['newspaper'] = t[3]
+	t[0]['blocks'] = t[4]
 
 def p_nNewspaper(t):
 	'''nNewspaper : NEWSPAPER LBRACE nNewspaper_block RBRACE'''
@@ -143,32 +147,37 @@ def p_nNewspaper(t):
 
 def p_nNewspaper_block(t):
 	'''nNewspaper_block : nn_block nTitle nn_block'''
-	string = '<div id="header"><div id="logo"> <h1><a><span>%s</span></a></h1><p>%s</p></div></div>'
-	if (t[1]):
-		t[0] = string % (t[2], t[1])
-	else:
-		t[0] = string % (t[2], t[3])
+	t[0] = dict()
+	t[0]['title'] = t[2]['value']
+	t[0]['date'] = t[1]['value'] if t[1]['value'] else t[2]['value']
 
 def p_nn_block(t):
 	'''nn_block : nDate
 				| empty'''
-	t[0] = t[1] if t[1] else ''
+	t[0] = t[1] if t[1] else {'value':''}
 
 def p_blocks(t):
 	'''blocks : blocks block
 				| block
 				| empty'''
-	t[0] = t[1] + t[2] if len(t) == 3 else t[1]
+	if len(t) == 3 and t[2]:
+		t[1].extend(t[2])
+	t[0] = t[1]
 
 def p_block(t):
 	'''block : NAME LBRACE cblock RBRACE'''
-	t[0] = t[3]
-	#TODO: Verify if cblock have nTitle and nAbstract
+	t[3]['name'] = t[1]
+	if not (t[3].has_key('title') and t[3].has_key('abstract')):
+		print 'title or abstract is missing'
+		sys.exit(1)
+	t[0] = [t[3]]
 
 def p_cblock(t):
 	'''cblock : cblock ccblock 
 				| ccblock'''
-	t[0] = t[1] if len(t) == 2 else t[1] + t[2]
+	if len(t) == 3:
+		t[1].update(t[2])
+	t[0] = t[1]
 
 def p_ccblock(t):
 	'''ccblock :  nTitle
@@ -179,37 +188,51 @@ def p_ccblock(t):
 				| nAuthor
 				| nText
 				'''
-	t[0] = t[1]
+	t[0] = {t[1]['type']: t[1]['value']}
 
 def p_nTitle(t):
 	'''nTitle : TITLE COLON noWikiText'''
-	t[0] = t[3]
+	t[0] = dict()
+	t[0]['type'] = 'title'
+	t[0]['value'] = t[3]
 
 def p_nDate(t):
 	'''nDate : DATE COLON noWikiText'''
-	t[0] = t[3]
+	t[0] = dict()
+	t[0]['type'] = 'date'
+	t[0]['value'] = t[3]
 	#TODO: Complete this function
 
 def p_nAbstract(t):
 	'''nAbstract : ABSTRACT COLON noWikiText'''
-	t[0] = t[3]
+	t[0] = dict()
+	t[0]['type'] = 'abstract'
+	t[0]['value'] = t[3]
 
 def p_nImage(t):
 	'''nImage : IMAGE COLON POINT BAR noWikiText POINT PHOTO_EXT'''
-	t[0] = t[3] + t[4] + t[5] + t[6] + t[7]
+	t[0] = dict()
+	t[0]['type'] = 'image'
+	t[0]['value'] = t[3] + t[4] + t[5] + t[6] + t[7]
 
 def p_nSource(t):
 	'''nSource : SOURCE COLON noWikiText
 				| SOURCE COLON URL'''
-	t[0] = t[3]
+	t[0] = dict()
+	t[0]['type'] = 'source'
+	t[0]['value'] = t[3]
 
 def p_nAuthor(t):
 	'''nAuthor : AUTHOR COLON noWikiText'''
-	t[0] = t[3]
+	t[0] = dict()
+	t[0]['type'] = 'author'
+	t[0]['value'] = t[3]
 
 def p_nText(t):
 	'''nText : TEXT COLON wikiText'''
-	t[0] = t[3]
+	t[0] = dict()
+	t[0]['type'] = 'text'
+	t[0]['value'] = t[3]
 
 def p_wText(t):
 	'''wText : NAME
